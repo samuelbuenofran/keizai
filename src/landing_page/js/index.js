bfb0b1cbfe270b59d803a623988bec0bf7d456fe
Samuel Bueno Francisco <samuel.buenofran@gmail.com>
2023-04-26 22:16:51 -0300

    Added JS show login tab function

---
+++ src/landing_page/js/index.js
@@ -0,0 +1,6 @@
+// The following function exhibits the login tab to the user interface.
+
+showLoginTab = () => {
+  document.getElementById("login_tab").style.display = "block";
+  document.getElementById("signup_tab").style.display = "none";
+};
